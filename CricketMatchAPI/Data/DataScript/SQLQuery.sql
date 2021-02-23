c6c57ebef8df93665e41d663503c9a60e77a82ae
Manik Manik <manik7696591290@gmail.com>
2021-02-23 13:29:45 +1300

    script

---
+++ CricketMatchAPI/Data/DataScript/SQLQuery.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[Player] ON
+INSERT INTO [dbo].[Player] ([Id], [player_Name], [player_Email], [player_Score], [player_Age]) VALUES (6, N'Virat', N'virat6577@gmail.com', N'2776', N'33')
+INSERT INTO [dbo].[Player] ([Id], [player_Name], [player_Email], [player_Score], [player_Age]) VALUES (7, N'MSDhoni', N'dhoni879@gmail.com', N'4898', N'39')
+INSERT INTO [dbo].[Player] ([Id], [player_Name], [player_Email], [player_Score], [player_Age]) VALUES (8, N'Hardik', N'pandya344@gmail.com', N'1788', N'29')
+INSERT INTO [dbo].[Player] ([Id], [player_Name], [player_Email], [player_Score], [player_Age]) VALUES (9, N'Rohit', N'sharma789@gmail.com', N'4566', N'37')
+INSERT INTO [dbo].[Player] ([Id], [player_Name], [player_Email], [player_Score], [player_Age]) VALUES (10, N'Bumrah', N'jasprit65@gmail.com', N'589', N'22')
+SET IDENTITY_INSERT [dbo].[Player] OFF
